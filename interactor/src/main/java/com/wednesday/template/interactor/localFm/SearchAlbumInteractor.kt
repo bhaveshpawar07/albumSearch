a5d69552d70fdb057da7c32b48b017149857635a
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-23 17:55:12 +0530

    Added flow to the interactor

--- interactor/src/main/java/com/wednesday/template/interactor/localFm/SearchAlbumInteractor.kt
+++ interactor/src/main/java/com/wednesday/template/interactor/localFm/SearchAlbumInteractor.kt
@@ -1,5 +1,9 @@
 package com.wednesday.template.interactor.localFm
 
+import com.wednesday.template.domain.lastFm.Album
+import kotlinx.coroutines.flow.Flow
+
 interface SearchAlbumInteractor {
+    val searchResultsFlow: Flow<List<Album>>
     suspend fun search(searchTerm: String)
 }
