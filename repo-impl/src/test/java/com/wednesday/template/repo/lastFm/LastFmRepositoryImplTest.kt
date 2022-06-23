043848ee15969fcb99e9828df3366b025aee5a13
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-23 14:43:17 +0530

    Removed qualifier name for verifyNoMoreInteractions

--- repo-impl/src/test/java/com/wednesday/template/repo/lastFm/LastFmRepositoryImplTest.kt
+++ repo-impl/src/test/java/com/wednesday/template/repo/lastFm/LastFmRepositoryImplTest.kt
@@ -29,7 +29,7 @@ class LastFmRepositoryImplTest {
     }
 
     private fun verifyNoMoreInteractions(){
-        org.mockito.kotlin.verifyNoMoreInteractions(
+        verifyNoMoreInteractions(
             lastFmRemoteService,
             domainAlbumMapper
         )
