be64cfca607d5dc62124c5f5d8a0f048e00fe9e3
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-23 17:56:47 +0530

    Created test for the interactor flow

---
+++ interactor-impl/src/test/java/com/wednesday/template/interactor/lastFm/search/SearchAlbumInteractorImplTest.kt
@@ -0,0 +1,72 @@
+package com.wednesday.template.interactor.lastFm.search
+
+import app.cash.turbine.test
+import com.wednesday.template.domain.base.Result
+import com.wednesday.template.domain.lastFm.SearchAlbumUseCase
+import com.wednesday.template.interactor.base.CoroutineContextController
+import com.wednesday.template.interactor.base.InteractorTest
+import com.wednesday.template.interactor.lastFm.search.model.album
+import com.wednesday.template.interactor.localFm.search.SearchAlbumInteractorImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.same
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+@ExperimentalCoroutinesApi
+class SearchAlbumInteractorImplTest : InteractorTest() {
+    private lateinit var searchAlbumUseCase: SearchAlbumUseCase
+    private lateinit var coroutineContextController: CoroutineContextController
+    private lateinit var interactor : SearchAlbumInteractorImpl
+
+    @Before
+    fun setUp(){
+        searchAlbumUseCase= mock()
+        coroutineContextController = coroutineDispatcherRule.coroutineContextController
+    }
+
+    private fun verifyNoMoreInteractions(){
+        verifyNoMoreInteractions(
+            searchAlbumUseCase
+        )
+    }
+
+    private fun createInteractor(){
+        interactor = SearchAlbumInteractorImpl(searchAlbumUseCase,coroutineContextController)
+    }
+
+    @Test
+    fun `Given no error occurs, When search called, Then it returns flow list of albums`():Unit =
+        runTest {
+            //Given
+            val searchTerm = "Test"
+            val albumList = listOf(album)
+            whenever(searchAlbumUseCase(searchTerm)).thenReturn(Result.Success(albumList))
+
+            launchInTestScope {
+                createInteractor()
+            //when
+                interactor.searchResultsFlow.test {
+                    interactor.search(searchTerm)
+
+                    val result = awaitItem()
+
+                    //Then
+                    assertEquals(actual = result, expected = albumList)
+                    verify(searchAlbumUseCase, times(1)).invoke(same(searchTerm))
+                    verifyNoMoreInteractions()
+                    cancelAndIgnoreRemainingEvents()
+                }
+            }
+
+        }
+
+}
