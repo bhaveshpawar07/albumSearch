037dbae5763e0024e1bfae8197fdebf09dfbab9a
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-28 18:15:46 +0530

    mappers for newly added code

--- repo-impl/src/main/java/com/wednesday/template/repo/lastFm/DomainAlbumMapper.kt
+++ repo-impl/src/main/java/com/wednesday/template/repo/lastFm/DomainAlbumMapper.kt
@@ -2,10 +2,13 @@ package com.wednesday.template.repo.lastFm
 
 import com.wednesday.template.domain.lastFm.Album
 import com.wednesday.template.repo.util.Mapper
+import com.wednesday.template.service.lastFm.Local.LocalAlbumDetails
 import com.wednesday.template.service.lastFm.Remote.RemoteAlbum
 
-interface DomainAlbumMapper : Mapper<RemoteAlbum, Album>
-
+interface DomainAlbumMapper : Mapper<RemoteAlbum, Album>{
+   fun mapLocalAlbum(from: LocalAlbumDetails):Album
+   fun mapLocalAlbum(from: List<LocalAlbumDetails>):List<Album>  = from.map(::mapLocalAlbum)
+}
 class DomainAlbumMapperImpl : DomainAlbumMapper {
 
     override fun map(from: RemoteAlbum): Album {
@@ -22,6 +25,23 @@ class DomainAlbumMapperImpl : DomainAlbumMapper {
             xlImageSize = from.image[3].size,
             xlImageText = from.image[3].text,
             url = from.url
+        )
+    }
+
+    override fun mapLocalAlbum(from: LocalAlbumDetails): Album {
+        return Album(
+            mbid = from.id,
+            artist = from.artist,
+            name = from.name,
+            sImageSize = "small",
+            sImageText = from.sImageUrl,
+            mImageSize = "medium",
+            mImageText = from.mImageUrl,
+            lImageSize = "large",
+            lImageText = from.lImageUrl,
+            xlImageSize = "extralarge",
+            xlImageText = from.xlImageUrl,
+            url = from.albumUrl
         )
     }
 }
