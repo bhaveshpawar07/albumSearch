ebc609d9f0683d4f31323e6a781f6ed53ce001fb
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-28 18:27:00 +0530

    Wildcard imports removed

--- domain-di/src/main/java/com/wednesday/template/domain/DomainModule.kt
+++ domain-di/src/main/java/com/wednesday/template/domain/DomainModule.kt
@@ -4,7 +4,16 @@ import com.wednesday.template.domain.base.datetime.ConvertDateToLongUseCase
 import com.wednesday.template.domain.base.datetime.ConvertDateToLongUseCaseImpl
 import com.wednesday.template.domain.base.datetime.FormatDateUseCase
 import com.wednesday.template.domain.base.datetime.FormatDateUseCaseImpl
-import com.wednesday.template.domain.lastFm.*
+import com.wednesday.template.domain.lastFm.GetFavouriteAlbumUseCase
+import com.wednesday.template.domain.lastFm.GetFavouriteAlbumFlowUseCase
+import com.wednesday.template.domain.lastFm.GetFavouriteAlbumFlowUseCaseImpl
+import com.wednesday.template.domain.lastFm.GetFavouriteAlbumUseCaseImpl
+import com.wednesday.template.domain.lastFm.SetAlbumFavouriteUseCase
+import com.wednesday.template.domain.lastFm.SetAlbumFavouriteUseCaseImpl
+import com.wednesday.template.domain.lastFm.RemoveAlbumFavouriteUseCase
+import com.wednesday.template.domain.lastFm.RemoveAlbumFavouriteUseCaseImpl
+import com.wednesday.template.domain.lastFm.SearchAlbumUseCase
+import com.wednesday.template.domain.lastFm.SearchAlbumUseCaseImpl
 import com.wednesday.template.domain.weather.FetchFavouriteCitiesWeatherUseCase
 import com.wednesday.template.domain.weather.FetchFavouriteCitiesWeatherUseCaseImpl
 import com.wednesday.template.domain.weather.GetFavouriteCitiesFlowUseCase
