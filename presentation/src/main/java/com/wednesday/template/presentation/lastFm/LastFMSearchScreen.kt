b533d3cc10dc244a9eebbe51953420c116d30691
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-29 15:56:32 +0530

    disabled filename check

--- presentation/src/main/java/com/wednesday/template/presentation/lastFm/LastFMSearchScreen.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/lastFm/LastFMSearchScreen.kt
@@ -3,6 +3,7 @@ package com.wednesday.template.presentation.lastFm
 import com.wednesday.template.presentation.R
 import com.wednesday.template.presentation.screen.MainScreen
 import kotlinx.parcelize.Parcelize
+// ktlint-disable filename
 
 @Parcelize
 object LastFmSearchScreen : MainScreen(R.id.lastFMsearchFragment)
