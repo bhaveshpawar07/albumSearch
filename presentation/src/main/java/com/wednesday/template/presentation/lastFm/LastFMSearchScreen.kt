046d039f715b84b51f2701524adfa5abf61931a3
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-06-29 16:22:08 +0530

    added filename disable

--- presentation/src/main/java/com/wednesday/template/presentation/lastFm/LastFMSearchScreen.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/lastFm/LastFMSearchScreen.kt
@@ -1,9 +1,11 @@
+// ktlint-disable filename
 package com.wednesday.template.presentation.lastFm
 
 import com.wednesday.template.presentation.R
 import com.wednesday.template.presentation.screen.MainScreen
 import kotlinx.parcelize.Parcelize
-// ktlint-disable filename
+
 
 @Parcelize
 object LastFmSearchScreen : MainScreen(R.id.lastFMsearchFragment)
+
